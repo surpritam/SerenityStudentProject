e7fd226e958bdb27575cbdc17dee6e1530841cb1
Pritam Sur <pritamsur@Pritams-MBP.home>
2018-05-13 13:51:40 -0400

    Updated port number to 8080

--- studentApp/src/test/java/com/studentApp/testbase/TestBase.java
+++ studentApp/src/test/java/com/studentApp/testbase/TestBase.java
@@ -8,7 +8,7 @@ public class TestBase {
     @BeforeClass
     public static void init() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8085;
+        RestAssured.port = 8080;
         RestAssured.basePath = "/student";
     }
 }
